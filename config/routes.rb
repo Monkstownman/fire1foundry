65b868d0821f9c9119ae36a823c06e4864c2b823
johncog <johnacogan@gmail.com>
2016-07-09 08:10:22 +0100

    updating route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'dashboard/index'
-  root 'dashbard#index'
+  root 'dashboard#index'
   #root 'static_pages#home'
 end
