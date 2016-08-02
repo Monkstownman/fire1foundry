1fcb7bc6ec455d4e66d89e4947cb0c43a87a2b7f
johncog <johnacogan@gmail.com>
2016-08-02 14:42:24 +0100

    fixing routes to work on aws

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   resources :lookups
   resources :measures
   resources :users
+  resources :dashboard
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'dashboard/index'
-  root 'dashboard#index'
-  #root 'static_pages#home'
+  root 'lookups#index'
 end
